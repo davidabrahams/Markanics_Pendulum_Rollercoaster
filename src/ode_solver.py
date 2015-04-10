8f5a3f55cec9839abb3956b3e70027f43f193ff2
David <david.abrahams@students.olin.edu>
2015-04-10 15:55:59 -0400

    energy conservation

--- src/ode_solver.py
+++ src/ode_solver.py
@@ -1,43 +1,66 @@
 import math
 from numpy import array
+from numpy import linalg
+from numpy import dot
+from numpy import sin, cos
 from scipy.integrate import ode
 
 __author__ = 'davidabrahams'
 
 
 class ODESolver:
-
     g = 9.8
 
     def __init__(self):
-        self.m = 1
+        self.m_c = 2
+        self.m_r = 1
         self.dt = 1 / 24.0
+        self.l = 5
         self.t_0 = 0
         self.t_f = 20
 
     def f_x(self, x):
-        return 10 * math.cos(x) + 0.25 * (x - 10) ** 2
+        return 5 * cos(x) + 0.25 * (x - 10) ** 2
 
 
     def f_prime_x(self, x):
-        return -10 * math.sin(x) + .5 * (x - 10)
+        return -5 * sin(x) + .5 * (x - 10)
 
 
     def f_double_prime(self, x):
-        return -10 * math.cos(x) + .5
+        return -5 * cos(x) + .5
 
 
     def solve_ode(self):
-
         def derivs(t, W):
-            x, x_dot = W
-            x_double_dot = (-ODESolver.g * self.f_prime_x(x) - self.f_double_prime(x) * self.f_prime_x(
-                x) * x_dot ** 2) / (self.f_prime_x(x) ** 2 + 1)
-            return [x_dot, x_double_dot]
+            x, x_dot, theta, theta_dot = W
+            f_p = self.f_prime_x(x)
+            f_pp = self.f_double_prime(x)
+
+            # equations in the form [x_double_dot, theta_double_dot, T, N]
+            eqn1 = [1, 0, -math.cos(theta) / self.m_c, f_p / (self.m_c * math.sqrt(1 + f_p ** 2))]
+            rhs1 = [0]
+            eqn2 = [f_p, 0, -math.sin(theta) / self.m_c, -1 / (self.m_c * math.sqrt(1 + f_p ** 2))]
+            rhs2 = [-f_pp*x_dot**2 - ODESolver.g]
+            eqn3 = [1, -self.l*math.sin(theta), math.cos(theta)/self.m_r, 0]
+            rhs3 = [self.l*theta_dot**2 *math.cos(theta)]
+            eqn4 = [f_p, self.l*math.cos(theta), math.sin(theta)/self.m_r, 0]
+            rhs4 = [-f_pp*x_dot**2 + self.l * theta_dot**2 * math.sin(theta) - ODESolver.g]
+
+            M = array([eqn1, eqn2, eqn3, eqn4])
+            RHS = array([rhs1, rhs2, rhs3, rhs4])
+
+            solved = dot(linalg.inv(M), RHS)
+            x_double_dot = solved[0, 0]
+            theta_double_dot = solved[1, 0]
+
+            return [x_dot, x_double_dot, theta_dot, theta_double_dot]
 
         x_0 = 0
-        x_dot_0 = 1
-        initials = [x_0, x_dot_0]
+        x_dot_0 = 0
+        theta_0 = -math.pi / 2
+        theta_dot_0 = 0
+        initials = [x_0, x_dot_0, theta_0, theta_dot_0]
 
         T = []
         Solns = []
--- src/roller_coaster.py
+++ src/roller_coaster.py
@@ -1,5 +1,4 @@
-import math
-from numpy import array
+from numpy import array, linspace, cos, sin
 from scipy.integrate import ode
 from ode_solver import ODESolver
 from matplotlib import pyplot as plt
@@ -8,33 +7,62 @@ from matplotlib import animation
 __author__ = 'davidabrahams'
 
 fig = plt.figure()
-ax = plt.axes()
-line, = ax.plot([], [], lw=2)
-point, = ax.plot(None, None, 'o', markersize = 10)
+ax_1 = plt.subplot2grid((3, 2), (0, 0), colspan=2, rowspan=2)
+ax_2 = plt.subplot2grid((3, 2), (2, 0), colspan=2)
+track, = ax_1.plot([], [], lw=3)
+cart, = ax_1.plot(None, None, 'o', markersize=10)
+rod, = ax_1.plot([], [], lw=2)
+rider, = ax_1.plot(None, None, 'o', markersize=7)
+gpe_line, = ax_2.plot([], [], lw=2, label='GPE')
+ke_line, = ax_2.plot([], [], lw=2, label='KE Cart')
+ke_rider, = ax_2.plot([], [], lw=2, label='KE Rider')
+sum_nrg_line, = ax_2.plot([], [], lw=2, label='Total NRG')
+
 
 
 def init():
-    line.set_data(x, y)
-    return line,
+    x_min, x_max = ax_1.get_xlim()
+    x_vals = linspace(x_min, x_max, num=200)
+    y_vals = solver.get_y_vals(x_vals)
+    track.set_data(x_vals, y_vals)
+    return track,
 
 
 def animate(i):
-    point.set_data(x[i], y[i])
-    return point,
+    x1, y1, x2, y2 = x_c[i], y_c[i], x_r[i], y_r[i]
+    cart.set_data(x1, y1)
+    rod.set_data([x1, x2], [y1, y2])
+    rider.set_data(x2, y2)
+    gpe_line.set_data(t[0:i], gpe[0:i])
+    ke_line.set_data(t[0:i], ke[0:i])
+    ke_rider.set_data(t[0:i], ke2[0:i])
+    sum_nrg_line.set_data(t[0:i], sum_nrg[0:i])
+    return cart, rod, rider, gpe_line, ke_line, ke_rider, sum_nrg_line
 
 
 if __name__ == '__main__':
     solver = ODESolver()
     solns, t = solver.solve_ode()
-    x, x_dot = solns[:, 0], solns[:, 1]
-    y = solver.get_y_vals(x)
-    x_range = max(x) - min(x)
-    y_range = max(y) - min(y)
+    x_c, x_dot, theta, theta_dot = solns[:, 0], solns[:, 1], solns[:, 2], solns[:, 3]
+    y_c = solver.get_y_vals(x_c)
+    x_r = x_c + solver.l * cos(theta)
+    y_r = y_c + solver.l * sin(theta)
+    y_dot = solver.f_prime_x(x_c) * x_dot
+    gpe = solver.m_c*ODESolver.g*y_c + solver.m_r*ODESolver.g*y_r
+    ke = 0.5 * solver.m_c * (x_dot**2 + y_dot**2)
+    ke2 = 0.5 * solver.m_r * ((x_dot - solver.l*theta_dot*sin(theta))**2 + (y_dot + solver.l*theta_dot*cos(theta))**2)
+    sum_nrg = gpe + ke + ke2
+    x_range = max(x_c) - min(x_c)
+    y_range = max(y_c) - min(y_c)
     screen_width = 1.2 * max(x_range, y_range)
-    x_display = .5 * (max(x) + min(x) - screen_width), .5 * (max(x) + min(x) + screen_width)
-    y_display = .5 * (max(y) + min(y) - screen_width), .5 * (max(y) + min(y) + screen_width)
-    ax.set_xlim(x_display)
-    ax.set_ylim(y_display)
+    x_display = .5 * (max(x_c) + min(x_c) - screen_width), .5 * (max(x_c) + min(x_c) + screen_width)
+    y_display = .5 * (max(y_c) + min(y_c) - screen_width), .5 * (max(y_c) + min(y_c) + screen_width)
+    ax_1.set_xlim(x_display)
+    ax_1.set_ylim(y_display)
+    ax_2.set_xlim((min(t), max(t)))
+    ax_2.set_ylim((1.2*min(gpe), 1.2*max(gpe)))
+    ax_2.legend()
     frame_num = int((solver.t_f - solver.t_0)/solver.dt)
     anim = animation.FuncAnimation(fig, animate, init_func=init, blit=True, frames=frame_num, interval=solver.dt * 1000)
+    ax_1.set_aspect('equal')
     plt.show()
